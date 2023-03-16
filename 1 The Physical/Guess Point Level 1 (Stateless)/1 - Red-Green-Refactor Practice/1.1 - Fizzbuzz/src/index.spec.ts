96ed2e728cebfc1d4650a24574467593952359f3
Jagdish Parihar <jatin6972@gmail.com>
2023-03-17 00:08:56 +0530

    REFACTOR: creating fizzbuzz instance before each test

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -1,29 +1,29 @@
 import FizzBuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it("fizzbuzz should be defined", () => {
-    const fizzbuzz = new FizzBuzz();
+  let fizzbuzz: FizzBuzz;
 
+  beforeEach(() => {
+    fizzbuzz = new FizzBuzz();
+  });
+
+  it("fizzbuzz should be defined", () => {
     expect(fizzbuzz).toBeDefined();
   });
 
   it("should return 1 when given input of 1", () => {
-    const fizzbuzz = new FizzBuzz();
-
     const response = fizzbuzz.get(1);
 
     expect(response).toEqual("1");
   });
 
   it("should return 2 when given input of 2", () => {
-    const fizzbuzz = new FizzBuzz();
     const response = fizzbuzz.get(2);
 
     expect(response).toEqual("2");
   });
 
-  it("should return 3 when given input of 3", () => {
-    const fizzbuzz = new FizzBuzz();
+  it("should return 'Fizz' when given input of 3", () => {
     const response = fizzbuzz.get(3);
 
     expect(response).toEqual("Fizz");
