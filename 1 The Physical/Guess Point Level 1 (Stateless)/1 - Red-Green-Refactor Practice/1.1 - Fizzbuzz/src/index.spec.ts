e586ded0ce7408d9877c31f1abf905772547ada5
Jagdish Parihar <jatin6972@gmail.com>
2023-03-16 23:54:32 +0530

    GREEN: fizzbuzz is defined

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,9 @@
+import FizzBuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
+  it("fizzbuzz should be defined", () => {
+    const fizzbuzz = new FizzBuzz();
 
-
+    expect(fizzbuzz).toBeDefined();
+  });
 });
