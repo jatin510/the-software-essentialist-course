1be8315cb72061f84b68ef6cb4f224130065a7c7
Jagdish Parihar <jatin6972@gmail.com>
2023-03-17 22:22:21 +0530

    REFACTOR:  code logic

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -1,13 +1,22 @@
 export default class FizzBuzz {
   get(input: number): string {
-    if (input % 3 == 0 && input % 5 == 0) {
-      return "FizzBuzz";
-    } else if (input % 3 == 0) {
-      return "Fizz";
-    } else if (input % 5 == 0) {
-      return "Buzz";
-    } else {
-      return input + "";
+    let s = "";
+
+    // handle number divisible by 3
+    if (input % 3 === 0) {
+      s += "Fizz";
     }
+
+    // handle number divisible by 5
+    if (input % 5 == 0) {
+      s += "Buzz";
+    }
+
+    // if number is not divisible by 3 or 5
+    if (s.length === 0) {
+      s += input;
+    }
+
+    return s;
   }
 }
