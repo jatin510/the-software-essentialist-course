d3892d1232d1a591dddf33691b2d9ad26c9df75e
Jagdish Parihar <jatin6972@gmail.com>
2023-03-18 14:11:34 +0530

    refactor: updated function name for validating password length

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,10 +1,10 @@
 export default class PasswordValidator {
   public validate(password: string): boolean {
     // validate password length
-    return this.hasValidateLength(password);
+    return this.validateLength(password);
   }
 
-  private hasValidateLength(password: string) {
+  private validateLength(password: string) {
     const passwordLength = password.length;
     return passwordLength >= 5 && passwordLength <= 15;
   }
