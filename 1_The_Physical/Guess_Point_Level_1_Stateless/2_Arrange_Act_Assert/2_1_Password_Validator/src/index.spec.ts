ed0ce5ead234276d1b7e8cf4db5a49f00b3363a5
Jagdish Parihar <jatin6972@gmail.com>
2023-03-18 13:58:03 +0530

    feat: Creating PasswordValidator class

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import PasswordValidator from "./index";
 
-describe('password validator', () => {
+describe("password validator", () => {
+  it("passwordValidator instance should be defined", () => {
+    const passwordValidator = new PasswordValidator();
 
-})
-
+    expect(passwordValidator).toBeDefined();
+  });
+});
