d5f6cf5717a56902585afbe0ff2034eb6c208e66
Jagdish Parihar <jatin6972@gmail.com>
2023-03-25 18:45:34 +0530

    feat: tells that a time range is a valid military time range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,16 @@
+import MilitaryTimeValidator from './index'
 
 describe('military time validator', () => {
 
 
+	const militaryTimeValidator = new MilitaryTimeValidator();
+
+	it('should tell "01:12 - 14:32" is valid military time  ',()=>{
+
+		const time = "01:12 - 14:32";
+
+		const response = militaryTimeValidator.isValidTime(time);
+
+		expect(response).toBe(true)
+	})
 })
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -0,0 +1,6 @@
+export default class MilitaryTimeValidator {
+
+	public isValidTime(time:string){
+		return true;
+	}
+}
