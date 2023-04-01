ebedf46249b9772801ba26607c214e93f8c1e5be
Jagdish Parihar <jatin6972@gmail.com>
2023-04-01 11:21:58 +0530

    refactor: updated logic for startHour and startMinute

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -3,8 +3,8 @@ export default class MilitaryTimeValidator {
     const MAX_HOUR = 23;
     const MAX_MINUTE = 59;
 
-    const startHour = Number.parseInt(time.slice(0, 2));
-    const startMinute = Number.parseInt(time.slice(3, 5));
+    const startHour = Number.parseInt(time.split("-")[0].split(":")[0]);
+    const startMinute = Number.parseInt(time.split("-")[0].split(":")[1]);
 
     const endHour = Number.parseInt(time.split("-")[1].split(":")[0]);
     const endMinute = Number.parseInt(time.split("-")[1].split(":")[1]);
