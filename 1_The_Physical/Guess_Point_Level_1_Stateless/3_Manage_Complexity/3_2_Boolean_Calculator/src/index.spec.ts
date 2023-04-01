baaab2c3fa464ec1ef3d84dff71e6e347c83f9ae
Jagdish Parihar <jatin6972@gmail.com>
2023-04-01 22:32:37 +0530

    feat: 'TRUE' expressions are evaluated as true

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,11 @@
+import BooleanCalculator from "./index";
 
-describe('boolean calculator', () => {
+describe("boolean calculator", () => {
+  it('"TRUE" should return "true"', () => {
+    const expression = "TRUE";
 
-})
+    const response = BooleanCalculator.evaluate(expression);
+
+    expect(response).toBeTruthy();
+  });
+});
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -0,0 +1,5 @@
+export default class BooleanCalculator{
+	public static evaluate(expression:string){
+		return true
+	}
+}
