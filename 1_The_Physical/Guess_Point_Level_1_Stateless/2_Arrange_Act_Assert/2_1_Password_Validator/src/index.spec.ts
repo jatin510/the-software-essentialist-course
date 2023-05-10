d5a97111702ccf771828867548edbc4d3116e83c
Jagdish Parihar <jatin6972@gmail.com>
2023-05-10 13:30:11 +0530

    returns successfull response when the password is of valid length

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -13,4 +13,29 @@ describe("password validator", () => {
     expect(response.errors.length).toEqual(1);
     expect(response.errors[0]).toEqual("InvalidLengthError");
   });
+
+  it("return an invalid length error for password length greater than 15", () => {
+    // arrange
+    const password = "hellohowareyouhopeyoudoingwell";
+
+    // act
+    const response = PasswordValidator.validate(password);
+
+    // assert
+    expect(response.result).toBe(false);
+    expect(response.errors.length).toEqual(1);
+    expect(response.errors[0]).toEqual("InvalidLengthError");
+  });
+
+  it("return result as true and response of errors array to be 0", () => {
+    // arrange
+    const password = "helloworld";
+
+    // act
+    const response = PasswordValidator.validate(password);
+
+    // assert
+    expect(response.result).toBe(true);
+    expect(response.errors.length).toEqual(0);
+  });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,5 +1,11 @@
 export class PasswordValidator {
   public static validate(password: string) {
+    const length = password.length;
+
+    if (length >= 5 && length <= 15) {
+      return { result: true, errors: [] };
+    }
+
     return { result: false, errors: ["InvalidLengthError"] };
   }
 }
