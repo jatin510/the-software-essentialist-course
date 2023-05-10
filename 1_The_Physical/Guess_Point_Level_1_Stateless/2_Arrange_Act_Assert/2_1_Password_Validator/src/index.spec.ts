542adb2b663767f734862d26d9d01c152a527681
Jagdish Parihar <jatin6972@gmail.com>
2023-05-10 13:53:54 +0530

    returns an error if the password does not contain any digit

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { PasswordValidator } from "./index";
+import { PasswordValidator, ValidatePasswordResponse } from "./index";
 
 describe("password validator", () => {
   it("it return an invalid length error for password length lesser than 5", () => {
@@ -6,12 +6,12 @@ describe("password validator", () => {
     const password = "mom";
 
     // act
-    const response = PasswordValidator.validate(password);
+    const response: ValidatePasswordResponse = PasswordValidator.validate(password);
 
     // assert
     expect(response.result).toBe(false);
-    expect(response.errors.length).toEqual(1);
-    expect(response.errors[0]).toEqual("InvalidLengthError");
+    expect(response.errors.length).toBeGreaterThanOrEqual(1);
+    expect(response.errors).toContain("InvalidLengthError");
   });
 
   it("return an invalid length error for password length greater than 15", () => {
@@ -19,23 +19,33 @@ describe("password validator", () => {
     const password = "hellohowareyouhopeyoudoingwell";
 
     // act
-    const response = PasswordValidator.validate(password);
+    const response: ValidatePasswordResponse = PasswordValidator.validate(password);
 
     // assert
     expect(response.result).toBe(false);
-    expect(response.errors.length).toEqual(1);
-    expect(response.errors[0]).toEqual("InvalidLengthError");
+    expect(response.errors.length).toBeGreaterThanOrEqual(1);
+    expect(response.errors).toContain("InvalidLengthError");
   });
 
   it("return result as true and response of errors array to be 0", () => {
     // arrange
-    const password = "helloworld";
+    const password = "hello1world";
 
     // act
-    const response = PasswordValidator.validate(password);
+    const response: ValidatePasswordResponse = PasswordValidator.validate(password);
 
     // assert
     expect(response.result).toBe(true);
     expect(response.errors.length).toEqual(0);
+  });
+
+  it("returns an error when the password does not contain atLeast 1 digit", () => {
+    const password = "helloworld";
+
+    const response: ValidatePasswordResponse = PasswordValidator.validate(password);
+
+    expect(response.result).toBe(false);
+    expect(response.errors.length).toBeGreaterThanOrEqual(1);
+    expect(response.errors).toContain("MissingDigitError");
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,15 +1,28 @@
+export type ValidatePasswordResponse = {
+  result: boolean;
+  errors: string[];
+};
+
 const isValidLength = (password: string): boolean => {
   const length = password.length;
 
   return length >= 5 && length <= 15;
 };
 
+const hasDigit = (password: string) => {
+  return password.split("").find((char: any) => !isNaN(char));
+};
+
 export class PasswordValidator {
   public static validate(password: string) {
-    if (isValidLength(password)) {
-      return { result: true, errors: [] };
+    let errors = [];
+    if (!isValidLength(password)) {
+      errors.push("InvalidLengthError");
     }
 
-    return { result: false, errors: ["InvalidLengthError"] };
+    if (!hasDigit(password)) {
+      errors.push("MissingDigitError");
+    }
+    return { result: errors.length === 0, errors };
   }
 }
